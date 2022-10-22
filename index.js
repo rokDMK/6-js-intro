4494beaf8c5eeb0e1f84eab7964ee11c59aad36f
Rokas <rokas.domkus@gmail.com>
2022-10-22 16:25:34 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log(123);
+console.log('Labas rytas');
+console.log('labas vakaras')
+console.log(2 + 2)
+console.log('2 + 2 =', 2 + 2)
+
+
+// Single line comment
+
+/*
+Multi
+line
+comment
+*/
+
+/*
+Temos:
+- kintamieji - informacijos isiminimas
+- salygos sakiniai - playginimas/patikrinimas
+- ciklai - pasikartojanciu proceduru kartojimas iki kol reikia
+- funkcijos/klases - pasikartojancios logikos surinkimas i viena vieta
+- event listeners - (fronto) stebejimas kada ir kokki veiksma padare vartotojas
+*/
